750d758745658a08f62944dda1b3e2d010aaae2f
STUDENT <rithikamahendran337>
2026-06-12 11:40:11 +0530

    sample

--- sample.js
+++ sample.js
@@ -8,3 +8,19 @@ console.log(a/b)//division
 console.log(a%b)//modulus
 console.log(a**b)//exponenttiation
 
+//Assignment
+console.log("assignment operator");
+
+var c=12
+console.log(c+=5)
+
+//Comparsion
+console.log("Comparsion Operator");
+console.log(a==b);
+console.log(a!=b);
+
+
+
+
+
+
