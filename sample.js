e2bba2dfc6ee209c2f71b54c6249b5caee982974
STUDENT <rithikamahendran337>
2026-06-12 12:10:04 +0530

    added sample.js

--- sample.js
+++ sample.js
@@ -1,4 +1,4 @@
-//Arthimetic
+// Arithmetic operator
 var a = 10;
 var b = 20;
 console.log(a+b)//addition
@@ -8,13 +8,13 @@ console.log(a/b)//division
 console.log(a%b)//modulus
 console.log(a**b)//exponenttiation
 
-//Assignment
+//Assignment operator
 console.log("assignment operator");
 
 var c=12
 console.log(c+=5)
 
-//Comparsion
+//Comparsion operator
 var a=11
 var b=12
 console.log("Comparsion Operator");
@@ -22,6 +22,19 @@ console.log(a==b);
 console.log(a!=b);
 console.log(a<=b);
 console.log(a>=b);
+
+//Logical operator
+var a=15
+var b=12
+console.log("Logical Operator");
+console.log(a> 0 && b > 0);
+console.log(a> 0 && b < 0);
+console.log(a< 0 || b < 0);
+console.log(!(a > 0));
+
+
+
+
 
 
 
