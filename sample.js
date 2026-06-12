5c837f422a0f1085416623acd2ab4efcc096a6e9
STUDENT <rithikamahendran337>
2026-06-12 11:04:37 +0530

    var

---
+++ sample.js
@@ -0,0 +1,10 @@
+//Arthimetic
+var a = 10;
+var b = 20;
+console.log(a+b)//addition
+console.log(a-b)//substraction
+console.log(a*b)//multiplication
+console.log(a/b)//division
+console.log(a%b)//modulus
+console.log(a**b)//exponenttiation
+
