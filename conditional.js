a203c42e16bac319941fa511d734c3be291a8502
STUDENT <rithikamahendran337>
2026-06-15 11:19:14 +0530

    loop

--- conditional.js
+++ conditional.js
@@ -1,8 +1,28 @@
 var colour = "white"
 
-elseif(colour == "yellow"){
+if(colour == "yellow"){
     console.log("sunflower");
 }
 elseif(colour=="Pink"){
     console.log("rose");
 }
+//switch statement
+var signal="white"
+
+switch(signal){
+    case"green":
+    console.log("go");
+    break;
+    
+    case"yellow":
+    console.log("wait");
+    break;
+
+    case"red":
+    console.log("stop");
+    break;
+
+    default:
+        console.log("invalid signal colour");       
+}
+
